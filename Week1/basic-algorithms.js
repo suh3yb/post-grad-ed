8adf18ffea95216cb6d955d9748441856c9a31f0
Suheyb Coskun <suheyb.c@gmail.com>
2020-04-05 16:36:48 +0200

    add week1 basic algorithms answers

---
+++ Week1/basic-algorithms.js
@@ -0,0 +1,249 @@
+//  1 Basic Algorithm Scripting: Convert Celsius to Fahrenheit
+//    The algorithm to convert from Celsius to Fahrenheit is
+//    the temperature in Celsius times 9/5, plus 32.
+
+//    You are given a variable celsius representing a temperature
+//    in Celsius. Use the variable fahrenheit already defined and
+//    assign it the Fahrenheit temperature equivalent to the given
+//    Celsius temperature. Use the algorithm mentioned above to help
+//    convert the Celsius temperature to Fahrenheit.
+
+function convertToF(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+convertToF(30);
+
+//  2 Basic Algorithm Scripting: Reverse a String
+//    Reverse the provided string.
+
+//    You may need to turn the string into an array before you can reverse it.
+//    Your result must be a string.
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString('hello');
+
+//  3 Basic Algorithm Scripting: Factorialize a Number
+//    Return the factorial of the provided integer.
+
+//    If the integer is represented with the letter n, a factorial
+//    is the product of all positive integers less than or equal to n.
+//    Factorials are often represented with the shorthand notation n!
+//    For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+//    Only integers greater than or equal to zero will be supplied to the function.
+
+function factorialize(num) {
+  if (num === 0) return 1;
+  return num * factorialize(num - 1);
+}
+
+factorialize(5);
+
+//  4 Basic Algorithm Scripting: Find the Longest Word in a String
+//    Return the length of the longest word in the provided sentence.
+//    Your response should be a number.
+
+const findLongestWordLength = (str) =>
+  str.split(' ').sort((a, b) => (a.length > b.length ? -1 : 1))[0].length;
+
+findLongestWordLength('The quick brown fox jumped over the lazy dog');
+
+//  5 Basic Algorithm Scripting: Return Largest Numbers in Arrays
+//    Return an array consisting of the largest number from each provided
+//    sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+//
+//    Remember, you can iterate through an array with a simple for loop,
+//    and access each member with array syntax arr[i].
+
+function largestOfFour(arr) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(arr[i].sort((a, b) => (a < b ? 1 : -1))[0]);
+  }
+  return newArray;
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
+
+//  6 Basic Algorithm Scripting: Confirm the Ending
+//    Check if a string (first argument, str) ends with the given
+//    target string (second argument, target).
+//
+//    This challenge can be solved with the .endsWith() method,
+//    which was introduced in ES2015. But for the purpose of
+//    this challenge, we would like you to use one of the
+//    JavaScript substring methods instead.
+
+function confirmEnding(str, target) {
+  return str.slice(target.length * -1) === target;
+}
+
+confirmEnding('Bastian', 'n');
+
+//  7 Basic Algorithm Scripting: Repeat a String Repeat a String
+//    Repeat a given string str (first argument) for num times
+//    (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  let newStr = '';
+  for (let i = num; i > 0; i--) {
+    newStr += str;
+  }
+  return newStr;
+}
+
+repeatStringNumTimes('abc', 3);
+
+//  8 Basic Algorithm Scripting: Truncate a String
+//    Truncate a string (first argument) if it is longer
+//    than the given maximum string length (second argument).
+//    Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  return str.slice(0, num) + (str.length > num ? '...' : '');
+}
+
+truncateString('A-tisket a-tasket A green and yellow basket', 8);
+
+//  9 Basic Algorithm Scripting: Finders Keepers
+//    Create a function that looks through an array
+//    (first argument) and returns the first element
+//    in the array that passes a truth test (second argument).
+//    If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+  return arr.filter((elem) => func(elem))[0];
+}
+
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
+
+// 10 Basic Algorithm Scripting: Boo who
+//    Check if a value is classified as a boolean
+//    primitive. Return true or false.
+//
+//    Boolean primitives are true and false.
+
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
+
+// 11 Basic Algorithm Scripting: Title Case a Sentence
+//    Return the provided string with the first letter of
+//    each word capitalized. Make sure the rest of the word is in lower case.
+//
+//    For the purpose of this exercise, you should also
+//    capitalize connecting words like "the" and "of".
+
+function titleCase(str) {
+  const wordsArr = [];
+  str
+    .toLowerCase()
+    .split(' ')
+    .map((elem) => {
+      wordsArr.push(elem[0].toUpperCase() + elem.slice(1));
+    });
+  return wordsArr.join(' ');
+}
+
+titleCase("I'm a little tea pot");
+
+// 12 Basic Algorithm Scripting: Slice and Splice
+//    You are given two arrays and an index.
+//
+//    Use the array methods slice and splice to copy each
+//    element of the first array into the second array, in order.
+//    Begin inserting elements at index n of the second array.
+//    Return the resulting array. The input arrays should
+//    remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  const newArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.splice(n + i, 0, arr1[i]);
+  }
+  return newArr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// 13 Basic Algorithm Scripting: Falsy BouncerPassed
+//    Remove all falsy values from an array.
+//
+//    Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+//    Hint: Try converting each value to a Boolean.
+
+const bouncer = (arr) => arr.filter((elem) => elem);
+
+bouncer([7, 'ate', '', false, 9]);
+
+// 14 Basic Algorithm Scripting: Where do I Belong
+//    Return the lowest index at which a value (second argument)
+//    should be inserted into an array (first argument) once it
+//    has been sorted. The returned value should be a number.
+//
+//    For example, getIndexToIns([1,2,3,4], 1.5) should return 1
+//    because it is greater than 1 (index 0), but less than 2 (index 1).
+//
+//    Likewise, getIndexToIns([20,3,5], 19) should return 2
+//    because once the array has been sorted it will look like
+//    [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  return arr.sort((a, b) => (a < b ? -1 : 1)).indexOf(num);
+}
+
+getIndexToIns([40, 60], 50);
+
+// 15 Basic Algorithm Scripting: Mutations
+//    Return true if the string in the first element of
+//    the array contains all of the letters of the string
+//    in the second element of the array.
+//
+//    For example, ["hello", "Hello"], should return true
+//    because all of the letters in the second string are
+//    present in the first, ignoring case.
+//
+//    The arguments ["hello", "hey"] should return false
+//    because the string "hello" does not contain a "y".
+//
+//    Lastly, ["Alien", "line"], should return true because
+//    all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+  const firstWord = arr[0].toLowerCase();
+  const secondWord = arr[1].toLowerCase();
+  for (let i = 0; i < secondWord.length; i++) {
+    if (!firstWord.includes(secondWord[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(['hello', 'hey']);
+
+// 16 Basic Algorithm Scripting: Chunky MonkeyPassed
+//    Write a function that splits an array (first argument)
+//    into groups the length of size (second argument) and
+//    returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  for (let i = 0; i < arr.length / size; i++) {
+    newArr.push(arr.slice(i * size, (i + 1) * size));
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(['a', 'b', 'c', 'd'], 2);
